9a6d28e062d81f9fd1778dd4002866e9fb61f056
Simon Champion <simonc@sfwltd.co.uk>
2017-03-03 17:14:26 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,72 @@
+Restrict Username
+-----------------
+
+A Joomla plugin to add character restrictions to the username field.
+
+If you like this extension, please review it at the Joomla Extensions Directory.
+
+Introduction
+------------
+
+This is a user plugin for Joomla that adds a validation layer to the username field.
+
+The plugin has a number of configuration parameters that allow you to specify exactly what this validation should look like.
+
+
+Version History
+----------------
+* 1.0.0     2016-07-03: Initial release.
+* 1.0.1     2016-07-14: Bug fixes: Now works in all three scenarios.
+* 2.0.0     2016-12-13: Made it work with the "Administrator" option for new account activation.
+* 2.1.0     2017-01-30: Added ability to send activation emails.
+* 2.2.0     2017-02-06: Activation emails can now go to either the end user or to admin users (or both).
+
+
+Installation
+----------------
+This is a standard Joomla plugin. Installation is via Joomla's extension manager.
+
+
+Usage
+----------------
+The plugin has a number of configuration parameters:
+
+* Min and Max length: These two parameters allow you to enforce a minimum and maximum length for usernames.
+
+* Custom error message: Use this to define the error message that will be displayed if the validation fails. If you leave it blank, the system will generate a default error message, which shows a bullet-point list of permitted character types. This default message can of course be modified as well, by using Joomla's language translation overrides, but will still be in bullet-point format. This custom error message field is useful if you want a differently formatted error message.
+
+* Use custom expression: Setting this parameter to 'Yes' will trigger the display of a 'Custom regular expression' free text field below it. This then allows you to enter a regular expression for the validation. If you are familiar with regular expression syntax, this will give you complete freedom to define your own username restrictions. This is an advanced option: If you are not familiar with regular expressions, you should leave this setting switched off and use the Yes/No options below it instead. Additionally, if you define a custom expression, you should also define a custom error message (see above), as the standard error message relies on the settings of the remaining fields.
+
+* Allow spaces, upper case, lower case, numbers, dots, dashes: These fields allow you to configure whether to allow these types of character in usernames.
+
+* Allow Cyrillic, Arabic, Hebrew, Extended Latin: These fields allow you to configure whether to allow characters from various languages in usernames.
+
+* Allow Symbols: Setting this to 'Yes' will trigger an additional field allowing to specify exactly which symbols to allow.
+
+* Allow email addresses: This option looks the same as the others in the config form, but internally it works separately in that it is validated using Joomla's built in email address validation, rather than using a regular expression. This check is done in addition to the other validations, so if you set this to allowed, you must also (at the very least) set Allow Symbols and include the '@' sign, otherwise it will fail on that even though you are allowing email addresses. Likewise, it is possible to switch this option off but still have an email address accepted as valid if your other options allow the characters.
+
+
+Limitations
+----------------
+
+
+
+Motivation
+----------------
+This plugin was written from scratch as a response to a request for a similar feature to be built into Joomla. See https://github.com/joomla/joomla-cms/issues/14275.
+
+
+To Do
+-----
+
+* Is the 'Allow Email' feature actually useful? Maybe remove it.
+* Could add other language scripts if requested.
+
+
+Caveats
+-------
+
+
+License
+----------------
+As with all Joomla extensions, this plugin is licensed under the GPL, in this case GPLv3. The full license document should have been included with the source code.
---
+++ restrictusername.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * @package   Restrictusername
+ * @type      Plugin (User)
+ * @version   1.0.0
+ * @author    Simon Champion
+ * @copyright (C) 2016 Simon Champion
+ * @license   GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+**/
+
+defined('_JEXEC') or die('Restricted access');
+
+class plgUserRestrictusername extends JPlugin
+{
+    const MIN_MIN = 2;  //lowest allowed value for minLength.
+
+    protected $autoloadLanguage = true;
+
+    /**
+     * @throws RuntimeException if the username is not valid according to the regex.
+     */
+    public function onUserBeforeSave($user, $isnew, $data)
+    {
+        if (!$data['username']) {
+            return true;
+        }
+
+        if ($this->params->get('allowEmail')) {
+            if (JFactory::getMailer()->ValidateAddress($data['username'])) {
+                return true;
+            }
+        }
+
+        $regex = $this->buildAllowedCharsRegex();
+        if($regex && !preg_match($regex, $data['username'])) {
+            throw new RuntimeException($this->allowedCharsErrorMessage());
+        }
+
+        return true;
+    }
+
+    protected function buildAllowedCharsRegex()
+    {
+        if($this->params->get('useOwnRegex')) {
+            return $this->params->get('userDefinedRegex');
+        }
+
+        $characterExpression = implode('', $this->regexesFromParamData());
+        if (!$characterExpression) { return ''; }
+
+        $max = (int)$this->params->get('maxLength', 0);
+        $min = (int)$this->params->get('minLength', 0);
+        if (!$min < self::MIN_MIN) { $min = self::MIN_MIN;}
+        if ($max < $min) { $max = 0; }
+
+        $lengthRange = "{".$min.",".($max?:'')."}";
+
+        return "/^[{$characterExpression}]{$lengthRange}$/";
+    }
+
+    protected function allowedCharsErrorMessage()
+    {
+        if($this->params->get('userDefinedErrorMessage')) {
+            return $this->params->get('userDefinedErrorMessage');
+        }
+
+        if($this->params->get('useRegex')) {
+            return JText::_('PLG_USER_RESTRICTUSERNAME_NOT_MATCH_REGEX');   //"Invalid Username"
+        }
+
+        $errorMessage = JText::_('PLG_USER_RESTRICTUSERNAME_NOT_MEET_CRITERIA');   //"Username does not meet the following criteria:"
+        $errorBullets = $this->messagesFromParamData();
+        return $errorMessage."<ul><li>".implode('</li><li>', $errorBullets)."</li></ul>";
+    }
+
+    protected function messagesFromParamData()
+    {
+        $output = [];
+        foreach($this->paramData() as $paramName=>$paramData) {
+            list($rex, $message) = $paramData;
+            if($this->params->get($paramName)) {
+                $output[] = $message;
+            }
+        }
+        return $output;
+    }
+
+    protected function regexesFromParamData()
+    {
+        $output = [];
+        foreach($this->paramData() as $paramName=>$paramData) {
+            list($rex, $message) = $paramData;
+            if($this->params->get($paramName)) {
+                $output[] = $rex;
+            }
+        }
+        return $output;
+    }
+
+    protected function paramData() {
+        $sym = $this->params->get('userDefinedSymbols');
+        return [
+            'allowSpaces'   => ['\s',  JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_SPACES')],
+            'allowUpper'    => ['A-Z', JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_UPPER')],
+            'allowLower'    => ['a-z', JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_LOWER')],
+            'allowNumbers'  => ['\d',  JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_NUMBERS')],
+            'allowDots'     => [preg_quote('.'), JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_DOTS')],
+            'allowDashes'   => [preg_quote('-'), JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_DASHES')],
+            'allowSymbols'  => [preg_quote($sym), JText::sprintf('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_SYMBOLS', htmlentities($sym))],
+            'allowCyrillic' => ['\p{Cyrillic}', JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_CYRILLIC')],
+            'allowArabic'   => ['\p{Arabic}', JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_ARABIC')],
+            'allowHebrew'   => ['\p{Hebrew}', JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_HEBREW')],
+            'allowLatin'    => ['\p{Latin}', JText::_('PLG_USER_RESTRICTUSERNAME_MAY_CONTAIN_LATIN')],
+
+            //no regex for these as they're validated separately, but still here so we can include them in the message.
+            'allowEmail'    => ['', JText::_('PLG_USER_RESTRICTUSERNAME_MAY_BE_EMAIL')],
+            'minLength'     => ['', JText::sprintf('PLG_USER_RESTRICTUSERNAME_MIN_LENGTH', (int)$this->params->get('minLength', 0))],
+            'maxLength'     => ['', JText::sprintf('PLG_USER_RESTRICTUSERNAME_MAX_LENGTH', (int)$this->params->get('maxLength', 0))],
+        ];
+    }
+}
+
